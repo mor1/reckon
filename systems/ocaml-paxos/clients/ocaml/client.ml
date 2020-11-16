1607c70e5c6c6e2ab7106bce38dc61c99eef8a8e
Chris Jensen <cjj39@cam.ac.uk>
2020-11-16 17:19:01 +0000

    Update client to async

--- systems/ocaml-paxos/clients/ocaml/client.ml
+++ systems/ocaml-paxos/clients/ocaml/client.ml
@@ -1,104 +1,85 @@
-
-open Core
+open! Core
+open! Async
 module OPC = Ocamlpaxos.Client
 
-module Client : Rcclient.S with type t = OPC.t = 
-struct 
+module Client : Rcclient.S with type t = OPC.t = struct
   module M = Rcclient.Messages
-  module L = Rcclient.Log
 
   type t = OPC.t
 
-  let put client key value cid start_time = 
+  let put client k v cid start_time =
     let open M in
     let st = Unix.gettimeofday () in
-    let%lwt err = 
-      match%lwt OPC.op_write client key value with
-      | Ok _res -> 
-        Lwt.return ""
-      | Error (`Msg e) -> 
-        L.err (fun m -> m "Operation failed with %s" e);
-        Lwt.return e
-    in 
+    let%bind err =
+      let open Ocamlpaxos.Types in
+      match%bind OPC.op_write client ~k ~v with
+      | Success -> return ""
+      | ReadSuccess _ -> return ""
+      | Failure -> return "Operation failed"
+    in
     let end_ = Unix.gettimeofday () in
-    Lwt.return {
-      response_time=end_-.st
-    ; client_start = st
-    ; queue_start = start_time
-    ; end_
-    ; clientid = cid
-    ; optype = ""
-    ; target = ""
-    ; err
-    }
+    return
+      {
+        response_time = end_ -. st;
+        client_start = st;
+        queue_start = start_time;
+        end_;
+        clientid = cid;
+        optype = "";
+        target = "";
+        err;
+      }
 
-  let get client key cid start_time = 
-    let open M in 
+  let get client key cid start_time =
+    let open M in
     let st = Unix.gettimeofday () in
-    let%lwt err =
-      match%lwt OPC.op_read client key with
-      | Ok _res -> 
-        Lwt.return ""
-      | Error (`Msg e) -> 
-        L.err (fun m -> m "Operation failed with %s" e);
-        Lwt.return e
-    in 
+    let%bind err =
+      let open Ocamlpaxos.Types in
+      match%bind OPC.op_read client key with
+      | Success -> return ""
+      | ReadSuccess _ -> return ""
+      | Failure -> return "Operation failed"
+    in
     let end_ = Unix.gettimeofday () in
-    Lwt.return {
-      response_time=end_-.st
-    ; client_start = st
-    ; queue_start = start_time
-    ; end_
-    ; clientid = cid
-    ; optype = ""
-    ; target = ""
-    ; err
-    }
-end 
+    return
+      {
+        response_time = end_ -. st;
+        client_start = st;
+        queue_start = start_time;
+        end_;
+        clientid = cid;
+        optype = "";
+        target = "";
+        err;
+      }
+end
 
-module Test = Rcclient.Make(Client) 
+module Test = Rcclient.Make (Client)
 
-let main caps id result_pipe = 
-  let%lwt client = 
-    OPC.new_client ~cid:(Int32.to_int_exn id) ~client_files:caps ()
-  in Test.run client id result_pipe
+let main addrs id result_pipe =
+  let client = OPC.new_client addrs in
+  Test.run client id result_pipe
 
-let reporter =
-  let report src level ~over k msgf =
-    let k _ = over () ; k () in
-    let src = Logs.Src.name src in
-    msgf
-    @@ fun ?header ?tags:_ fmt ->
-    Fmt.kpf k Fmt.stdout
-      ("[%a] %a %a @[" ^^ fmt ^^ "@]@.")
-      Time.pp (Time.now ())
-      Fmt.(styled `Magenta string)
-      (Printf.sprintf "%14s" src)
-      Logs_fmt.pp_header (level, header)
-  in
-  {Logs.report}
+let node_list = Command.Arg_type.create @@ String.split ~on:','
 
-let node_list =
-  Command.Arg_type.create @@ String.split ~on:','
+let int32 = Command.Arg_type.create Int32.of_string
 
-let int32 = 
-  Command.Arg_type.create Int32.of_string
-
-let command = 
-  Core.Command.basic ~summary:"Client Spawner for OcamlPaxos"
-    Core.Command.Let_syntax.(
-      let%map_open caps = anon ("cap_files" %: node_list) 
+let command =
+  Command.async ~summary:"Client Spawner for OcamlPaxos"
+    Command.Let_syntax.(
+      let%map_open addrs = anon ("cap_files" %: node_list)
       and clientid = anon ("client_id" %: int32)
-      and result_pipe = anon ("result_pipe" %: string)
-      in 
+      and result_pipe = anon ("result_pipe" %: string) in
       fun () ->
-        Lwt_main.run @@ main caps clientid result_pipe
-    )
+        let open Ocamlpaxos in
+        let open Rcclient in
+        let global_level = Async.Log.Global.level () in
+        let global_output = Async.Log.Global.get_output () in
+        List.iter [ Client.logger; Rcclient.logger ] ~f:(fun log ->
+            Async.Log.set_level log global_level;
+            Async.Log.set_output log global_output);
+        main addrs clientid result_pipe)
 
-let () = 
-  Lwt_engine.set (new Lwt_engine.libev ());
-  Fmt_tty.setup_std_outputs () ;
-  Logs.(set_level (Some Debug));
-  Logs.Src.set_level Capnp_rpc.Debug.src (Some Info);
-  Logs.set_reporter reporter;
+let () =
+  Fmt_tty.setup_std_outputs ();
   Core.Command.run command
