0af02e30a01a6ef48a75e3808e2061f47cbaae27
Chris Jensen <cjj39@cam.ac.uk>
2020-11-16 16:57:55 +0000

    Update to Async

--- src/ocaml_client/rcclient/rcclient.ml
+++ src/ocaml_client/rcclient/rcclient.ml
@@ -1,34 +1,38 @@
+open! Core
+open! Async
+open! Ppx_log_async
+
 
 module Messages = Message_types
-
-let src = Logs.Src.create "client" ~doc:("Resolving consensus test logger")
-
-module Log = (val Logs.src_log src : Logs.LOG)
 
 module type S = sig
   type t
 
-  val put : t -> bytes -> bytes -> int32 -> float -> Message_types.response Lwt.t
-  val get : t -> bytes -> int32 -> float -> Message_types.response Lwt.t
+  val put : t -> bytes -> bytes -> int32 -> float -> Message_types.response Deferred.t
+  val get : t -> bytes -> int32 -> float -> Message_types.response Deferred.t
 end 
 
+let logger =
+  let open Async_unix.Log in
+  create ~level:`Info ~output:[] ~on_error:`Raise
+    ~transform:(fun m -> Message.add_tags m [("src", "Rcclient")])
+    ()
 
 module Make (Cli : S) : sig
   type client = Cli.t
-  val run : client -> int32 -> string -> unit Lwt.t 
+  val run : client -> int32 -> string -> unit Deferred.t 
 end = struct
   module EB = EndianBytes.LittleEndian
   module MT = Message_types
   module MP = Message_pb
   module PD = Pbrt.Decoder
   module PE = Pbrt.Encoder
-  module L = Log
 
   type client = Cli.t
 
   let perform op client cid start_time i = 
-    L.debug (fun m -> m "Submitting request %d" i);
-    let%lwt res = 
+    [%log.debug logger "Submitting request" (i: int)];
+    let%bind res = 
       let keybuf = Bytes.create 8 in
       match op with
       | MT.Put {key; value} -> (
@@ -40,84 +44,97 @@ end = struct
           Cli.get client keybuf cid start_time
         )
     in 
-    L.info (fun m -> m "Performed op %d" i);
-    Lwt.return res
+    [%log.debug logger "Performed" ~op:(i : int)];
+    return res
 
-  let recv_from_br (br : Lwt_io.input_channel) = 
+  let recv_from_br (br : Reader.t) = 
+    let open Deferred.Result in
+    let open Deferred.Result.Let_syntax in
     let rd_buf = Bytes.create 4 in
-    let%lwt () = Lwt_io.read_into_exactly br rd_buf 0 4 in
-    let size = EB.get_int32 rd_buf 0 |> Int32.to_int in
-    let payload_buf = Bytes.create size in
-    let%lwt () = Lwt_io.read_into_exactly br payload_buf 0 size in
-    Logs.debug (fun m -> m "received op size = %d\n" size);
-    MP.decode_request (PD.of_bytes payload_buf) |> Lwt.return
+    let%bind () = 
+      match%bind.Deferred Reader.really_read br rd_buf ~pos:0 ~len:4 with
+      | `Eof _ as v-> fail v 
+      | `Ok ->  return ()
+    in 
+    let len = EB.get_int32 rd_buf 0 |> Int32.to_int_exn in
+    let payload_buf = Bytes.create len in
+    let%bind () = 
+      match%bind.Deferred Reader.really_read br payload_buf ~pos:0 ~len 
+      with
+      | `Eof _ as v -> fail v
+      | `Ok -> return ()
+    in
+    [%log.debug logger "Received op" (len : int)];
+    MP.decode_request (PD.of_bytes payload_buf) |> return
 
   let send payload outfile =
     let p_len = Bytes.length payload in
     let payload_buf = Bytes.create (p_len + 4) in
-    Bytes.blit payload 0 payload_buf 4 p_len;
-    let p_len = Int32.of_int p_len in
+    Bytes.blit ~src:payload ~src_pos:0 ~dst:payload_buf ~dst_pos:4 ~len:p_len;
+    let p_len = Int32.of_int_exn p_len in
     EB.set_int32 payload_buf 0 p_len;
-    Lwt_io.write_from_exactly outfile payload_buf 0 (Bytes.length payload_buf)
+    Writer.write_bytes outfile payload_buf ~pos:0 ~len:(Bytes.length payload_buf)
 
   let result_loop res_list outfile =
     let iter v =
       let encoder = PE.create () in
       let payload = MP.encode_response v encoder; PE.to_bytes encoder in
-      send payload outfile
+      send payload outfile |> return
     in 
-    L.info (fun m -> m "Starting to return results");
-    let%lwt () = Lwt_list.iter_s iter res_list in
-    L.info (fun m -> m "Finished returning results");
-    Lwt.return_unit
+    [%log.info logger "Starting to return results"];
+    let%bind () = Deferred.List.iter ~how:`Sequential ~f:iter res_list in
+    [%log.info logger "Finished returning results"];
+    Deferred.unit
 
   let run client id result_pipe = 
-    let%lwt result_pipe = Lwt_io.open_file ~flags:[Unix.O_WRONLY] ~perm:0x0755 ~mode:Lwt_io.output result_pipe in
-    let in_pipe = Lwt_io.of_fd ~mode:Lwt_io.input Lwt_unix.stdin in
-    L.info (fun m -> m "PRELOAD - Start");
-    let stream = 
-      Lwt_stream.from (fun () -> 
-          match%lwt recv_from_br in_pipe with
-          | Finalise {msg} -> (L.debug (fun m -> m "Got finalise %s" msg); Lwt.return_none)
-          | Start {msg=_msg}-> failwith "Got start during preload phase"
-          | Op op -> Lwt.return_some op )
+    let%bind result_pipe = Writer.open_file ~perm:0x0755 result_pipe in
+    let in_pipe = Reader.create (Fd.stdin ()) in
+    [%log.info logger "PRELOAD - Start"];
+    let pipe = 
+      Pipe.unfold ~init:() ~f:(fun () ->
+          match%map recv_from_br in_pipe with
+          | Error _ -> None
+          | Ok v -> match v with
+            | Finalise {msg} -> (
+              [%log.debug logger "Got finaliase" (msg: string)];
+              None
+            )
+            | Start {msg=_msg}-> failwith "Got start during preload phase"
+            | Op op -> Some (op, ()) )
     in 
-    let%lwt op_list = 
-      Lwt_stream.fold_s (fun v ops -> 
+    let%bind op_list =
+      Pipe.fold pipe ~init:[] ~f:(fun ops v ->
           match v with
           | (MT.{prereq=true; _}) ->
-            let%lwt _res = perform v.op_type client id (Unix.gettimeofday ()) (-1) in 
-            Lwt.return ops
+            let%bind _res = perform v.op_type client id (Unix.gettimeofday ()) (-1) in 
+            return ops
           | _ -> 
-            v :: ops |> Lwt.return
-        ) stream [] 
+            v :: ops |> return
+        )
     in 
-    let op_list = List.rev op_list in
-    L.info (fun m -> m "READYING - Start");
-    let%lwt () = send (Bytes.create 0) result_pipe in
-    let%lwt _got_start = 
-      match%lwt recv_from_br in_pipe with
-      | Start {msg} ->
-        L.debug (fun m -> m "Starting with %s" msg);
-        Lwt.return_unit 
+    let op_list = List.sort op_list ~compare:(fun a b -> Float.compare a.start b.start) in
+    [%log.debug logger "READYING - Start"];
+    send (Bytes.create 0) result_pipe;
+    let%bind _got_start = 
+      match%bind recv_from_br in_pipe with
+      | Ok (Start {msg}) ->
+        [%log.debug logger "Starting test" (msg : string)];
+        Deferred.unit
       | _ -> failwith "Got something other than a ready!"
     in 
-    L.info (fun m -> m "EXECUTE - Start");
-    let start_time = Unix.gettimeofday() in
-    let iter_op_list ir res_list MT.{prereq=_; start; op_type=op} =
-      let i = !ir in
-      ir := i + 1;
-      let sleep_time = start +. start_time -. Unix.gettimeofday () in
-      let%lwt () = Lwt_unix.sleep sleep_time in
-      let%lwt res = perform op client id (Unix.gettimeofday ()) i in
-      res_list := res :: !res_list;
-      Lwt.return_unit
+    [%log.info logger "EXECUTE - Start"];
+    let foldi test_start i acc MT.{prereq=_; start=offset; op_type=op} =
+      let start = 
+        let open Time in
+        let offset = Span.of_sec offset in
+        add test_start offset
+      in
+      let%bind () = at start in
+      let res = perform op client id (Unix.gettimeofday ()) i in
+      return (res :: acc)
     in 
-    (*let%lwt res_list = Lwt_list.mapi_p iter_op_list op_list in*)
-    let op_stream = Lwt_stream.of_list op_list in
-    let ir = ref 1 in
-    let res_list = ref [] in
-    let%lwt () = Lwt_stream.iter_n ~max_concurrency:1024 (iter_op_list ir res_list) op_stream in
-    L.info (fun m -> m "Finished applying ops");
-    result_loop !res_list result_pipe
+    let%bind res = Deferred.List.foldi op_list ~init:[] ~f:(foldi @@ Time.now ()) in
+    let%bind res_list = res |> List.rev |> Deferred.List.all in
+    [%log.info logger "Finished applying ops"];
+    result_loop res_list result_pipe
 end 
